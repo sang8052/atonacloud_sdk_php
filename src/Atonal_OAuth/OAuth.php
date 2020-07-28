6ecdb1c564b15abe38c07d2a524f8dbc578ae718
sudemqaq <mail@szhcloud.cn>
2020-07-28 22:50:16 +0800

    v1.0 修复融合登录时 不支持设置安全参数的缺陷

--- src/Atonal_OAuth/OAuth.php
+++ src/Atonal_OAuth/OAuth.php
@@ -8,13 +8,16 @@ class OAuth extends AtonalAuth
 {
 
 
-    public function AppLogin($OAuthId,$OAuthApp,$OAuthSafe="",$OAuthCallBack="",$OAuthClientParam="",$OAuthServerParam=""){
+    public function AppLogin($OAuthId,$OAuthApp,$OAuthSafe="",$OAuthSafeParam=array(),$OAuthCallBack="",$OAuthClientParam="",$OAuthServerParam=""){
         if(!is_string($OAuthClientParam)) return "ERROR:OAuthClientParam Must A String!";
         if(!is_string($OAuthServerParam)) return "ERROR:OAuthServerParam Must A String!";
         $Param = array(
             "OAuthId" => $OAuthId,
             "OAuthApp" => $OAuthApp,
             "OAuthSafe" => $OAuthSafe,
+            "Safe_Reffer" => $OAuthSafeParam["Reffer"],
+            "Safe_Ip" => $OAuthSafeParam["Ip"],
+            "Safe_Ua" => $OAuthSafeParam["Ua"],
             "OAuthCallBack" => $OAuthCallBack,
             "OAuthClientParam" => $OAuthClientParam,
             "OAuthServerParam" => $OAuthServerParam
