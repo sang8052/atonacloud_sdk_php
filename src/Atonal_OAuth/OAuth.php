d0283835b508ae9ac04c57e449b6e79caaf131cc
sudemqaq <mail@szhcloud.cn>
2020-07-28 23:02:20 +0800

    修改OAuth 的参数

--- src/Atonal_OAuth/OAuth.php
+++ src/Atonal_OAuth/OAuth.php
@@ -8,16 +8,16 @@ class OAuth extends AtonalAuth
 {
 
 
-    public function AppLogin($OAuthId,$OAuthApp,$OAuthSafe="",$OAuthSafeParam=array(),$OAuthCallBack="",$OAuthClientParam="",$OAuthServerParam=""){
+    public function AppLogin($OAuthId,$OAuthApp,$OAuthSafe="",$OAuthSafe_Reffer="",$OAuthSafe_Ip="",$OAuthSafe_Ua="",$OAuthCallBack="",$OAuthClientParam="",$OAuthServerParam=""){
         if(!is_string($OAuthClientParam)) return "ERROR:OAuthClientParam Must A String!";
         if(!is_string($OAuthServerParam)) return "ERROR:OAuthServerParam Must A String!";
         $Param = array(
             "OAuthId" => $OAuthId,
             "OAuthApp" => $OAuthApp,
             "OAuthSafe" => $OAuthSafe,
-            "Safe_Reffer" => $OAuthSafeParam["Reffer"],
-            "Safe_Ip" => $OAuthSafeParam["Ip"],
-            "Safe_Ua" => $OAuthSafeParam["Ua"],
+            "Safe_Reffer" =>$OAuthSafe_Reffer,
+            "Safe_Ip" => $OAuthSafe_Ip,
+            "Safe_Ua" => $OAuthSafe_Ua,
             "OAuthCallBack" => $OAuthCallBack,
             "OAuthClientParam" => $OAuthClientParam,
             "OAuthServerParam" => $OAuthServerParam
